5d3536c7a95fd26b4056d83631ae9be1eeb2d70c
ALVAROPING1 <alvaroping1@gmail.com>
2025-01-29 13:52:22 +0100

    abort compilation on error

--- make.sh
+++ make.sh
@@ -1,9 +1,16 @@
+#!/bin/bash
+
 if [ "$1" != "" ]; then
     rm -rf build/
 fi
 
+
 mkdir build
 mkdir build/parts
+
+set -e
+
 latexmk --output-directory=build -cd -shell-escape -pdflua report.tex
 makeglossaries -d build report
 latexmk --output-directory=build -cd -shell-escape -pdflua report.tex
+xdg-open build/report.pdf
